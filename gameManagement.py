0d76dd4640de8f92276820494f14b904bfdc05f4
The_Gost_sniper <54660359+TheGostsniperfr@users.noreply.github.com>
2022-04-20 10:38:21 +0200

    Pong collision

--- gameManagement.py
+++ gameManagement.py
@@ -3,8 +3,8 @@
 # actu : test les colisions
 
 from math import *
-from fps_limiter import LimitFPS, FPSCounter
-
+from pickle import PERSID
+from random import choice
 
 
 """
@@ -15,11 +15,13 @@ quand la balle touche le mur alors sa fait un angle de 90°
 lorsqu'elle touche la raquette, l'angle varie en fonction de zone de la raquette (au milieu = renvoie tout droit, et plus on dessent ou monte, plus l'angle est grand )
 
 
+#le joueur 1 est a droite et le joueur 2 est a gauche
+
 """
 
 
 class GameProcessus:
-    def __init__(self, areaRange=[1000, 1000], defaultSpeed=50):
+    def __init__(self, areaRange=[1000, 1000], defaultSpeed=10, nbRound=8):
         self.areaRange = areaRange
         self.defaultSpeed = defaultSpeed
 
@@ -30,12 +32,15 @@ class GameProcessus:
         """
 
         # liste index :
-        self.topArea = ((0, self.areaRange[0]),(self.areaRange[1], self.areaRange[1]))
+        self.topArea = ((0, self.areaRange[0]),
+                        (self.areaRange[1], self.areaRange[1]))
         self.bottomArea = ((0, self.areaRange[0]), (0, 0))
         self.leftArea = ((0, 0), (0, self.areaRange[1]))
-        self.rightArea = ((self.areaRange[0], self.areaRange[0]), (self.areaRange[1], self.areaRange[1]))
+        self.rightArea = (
+            (self.areaRange[0], self.areaRange[0]), (self.areaRange[1], self.areaRange[1]))
 
-        #self.area = [self.topArea, self.bottomArea, self.leftArea, self.rightArea]
+        self.area = (self.topArea, self.bottomArea,
+                     self.leftArea, self.rightArea)
 
         # initialisation de la ball
 
@@ -43,59 +48,119 @@ class GameProcessus:
         self.ballPos = [self.areaRange[0]/2, self.areaRange[1]/2]
 
         # vector of ball + speed ( [x,y,s])
-        self.ballVector = [10, 3, self.defaultSpeed]
+        self.ballVector = [choice([i for i in range(-8, 8) if i not in [0]]), choice(
+            [i for i in range(-8, 8) if i not in [0]]), self.defaultSpeed]
 
-        # fonction qui détecte si la balle tocuhe un de la zone
+        # initialization raquettes, dimention raquette :( 10 x 100 ) :
+        self.raquettePos = [[[50+0, 50+10], [0, 100]],
+                            [[self.rightArea[0][0]-50-10, self.rightArea[0][0]-50], [0, 100]]]
 
-    def isTouched(self, newBallPosition):
+        #(scorePlayer1, scorePlayer2)
+        self.gameScore = [0, 0]
+
+    # fonction qui détecte si la balle tocuhe un de la zone
+
+    def isWallTouched(self, newBallPosition):
         # check si position x de la balle est = a une position d'un bord ( si oui alors ie : elle le touche)
 
         # Hit top
-        if(newBallPosition[0] >= self.topArea[1][0]):
+        if(newBallPosition[1] >= self.topArea[1][0]):
             print("Hit top")
-            self.ballVector[0] *= -1
+            self.ballVector[1] *= -1
             return True
 
         # Hit bottom
-        elif(newBallPosition[0] <= self.bottomArea[1][0]):
+        elif(newBallPosition[1] <= self.bottomArea[1][0]):
             print("Hit bottom")
-            self.ballVector[0] *= -1
+            self.ballVector[1] *= -1
             return True
 
-        # Hit left
-        elif(newBallPosition[1] <= self.leftArea[0][0]):
+        # Hit left = add point
+        elif(newBallPosition[0] <= self.leftArea[0][0]):
             print("Hit left")
-            self.ballVector[1] *= -1
+            self.ballVector[0] *= -1
+
+            # le joueur 1 a marqué
+            self.gameScore[0] += 1
+            self.newRound()
+
             return True
 
-        # Hit right
-        elif(newBallPosition[1] >= self.rightArea[0][0]):
+        # Hit right = add point
+        elif(newBallPosition[0] >= self.rightArea[0][0]):
             print("Hit right")
-            self.ballVector[1] *= -1
+            self.ballVector[0] *= -1
+
+            # le joueur 2 a marqué
+            self.gameScore[1] += 1
+            self.newRound()
+
             return True
 
         # No hit
         else:
             return False
 
-        """
-                    for i in range(len(self.area)):
+    def isRaquetteTouched(self, newBallPosition):
+        # on cherche a savoir si la balle touche la raquette
+        # si oui, alors on calcule en fonction de point de touche le nouveau vecteur de la balle
 
-            #check x ball hit
-            if(self.ballPos[0] in self.area[i][0]):
-                self.ballVector[0] *= -1
-                return self.ballVector
+        # parcours des deux raquettes des joueurs
+        for player in range(2):
+
+            # si la balle rentre dans la zone d'une raquettes
+            if(self.raquettePos[player][0][1] >= newBallPosition[0] >= self.raquettePos[player][0][0] and self.raquettePos[player][1][1] >= newBallPosition[1] >= self.raquettePos[player][1][0]):
             
-            #check y ball hit
-            elif(self.ballPos[1] in self.area[i][1]):
-                self.ballVector[0] *= -1
-                return self.ballVector
-            else:
-                return False
+                #debug :
+                print("raquette touché : ", player)
+                print("ballposition : ", self.ballPos)
+                print("ballVector : ", self.ballVector)
+                print("raquette : ", self.raquettePos)
 
-        """
+                print(self.raquettePos[player][0][0],
+                      newBallPosition[0], self.raquettePos[player][0][1])
+                print(self.raquettePos[player][1][1],
+                      newBallPosition[1], self.raquettePos[player][1][0])
+
+                # new vector after hit
+                """
+                this part of function have to role to determinate the new vector of the ball after hit the raquette of a player     
+                """
+
+                percentage = ( (newBallPosition[1] - self.raquettePos[player][1][0]) / (self.raquettePos[player][1][1] - self.raquettePos[player][1][0]) ) * 2
+
+                        
+
+                print("percentage : ", percentage)
+                print(newBallPosition[1], self.raquettePos[player][1][0], self.raquettePos[player][1][1], self.raquettePos[player][1][0])
+
+                if(percentage == 1):
+                    self.ballVector[0] = 0
+                    self.ballVector[1] *= -1
+
+                elif(percentage > 1):
+                    #limit of the ball angle
+                    if(percentage > 1.6):
+                        percentage = 1.6
+
+                    
+                    self.ballVector[0] *= -1
+                    self.ballVector[1] = percentage * (10)
+
+
+                elif(percentage < 1):
+                    #limit of the ball angle
+                    if(percentage < 0.4):
+                        percentage = 0.4
+
+                    self.ballVector[0] *= -1
+                    self.ballVector[1] = percentage * (-10)
+
+                return True
+
+        return False
+
     # fonction qui calcule les nouvelles coordonné de la balle
-
     def newBallPosition(self):
 
         # speed = nombre de fois que le vecteur sera multiplié : ( speed = self.ballVector[2] )
@@ -111,22 +176,78 @@ class GameProcessus:
                 position y actuelle + y du vecteur / par sa longeur
             
             """
-            print("speed : ", self.ballVector[2], i)
-            print("ballposition : ", self.ballPos)
-            print("ballVector : ", self.ballVector)
-            print("vectorNormalized : ", vectorNormalized)
 
-            tempoBallPosition = [self.ballPos[0] + self.ballVector[0] / vectorNormalized, self.ballPos[1] + self.ballVector[1] / vectorNormalized]
+            #print("speed : ", self.ballVector[2], i)
+            #print("ballposition : ", self.ballPos)
+            #print("ballVector : ", self.ballVector)
+            #print("raquette : ", self.raquettePos)
+            #print("vectorNormalized : ", vectorNormalized)
+            #print('score : ', self.gameScore)
 
-            # isTouched :
-            if(self.isTouched(tempoBallPosition) == True):
+            tempoBallPosition = [self.ballPos[0] + self.ballVector[0] /
+                                 vectorNormalized, self.ballPos[1] + self.ballVector[1] / vectorNormalized]
+
+            # isWallTouched :
+            if(self.isWallTouched(tempoBallPosition) == True):
                 print("mur touché !")
 
+            elif(self.isRaquetteTouched(tempoBallPosition) == True):
+                print("raquette touché :")
+
             else:
-                #la ball ne rentre pas en colision, elle continu son chemin
+                #print("continu son chemin")
+                # la ball ne rentre pas en colision, elle continu son chemin
                 self.ballPos = tempoBallPosition
-                #return self.ballPos
+                # return self.ballPos
         return self.ballPos
 
+    def moveRaquette(self, player, action):
 
+        # gestion des raquettes des deux joueurs
+        # si -> pressé alors on check si le bas de la bar touche
+        # si <- pressé alors on check si le haut de la bar touche
+        # si ça touche pas alors on applique le mouvement a x1 et x2
 
+        # si va vers le haut, check si le bas de la raquette touche
+        if(action < 0 and self.raquettePos[player][1][0] + action < self.bottomArea[1][0]):
+            # on colle la raquette au bas
+            self.raquettePos[player][1][0] = self.bottomArea[1][0]
+            # + taille de la raquette / à rajouter
+            self.raquettePos[player][1][1] = self.raquettePos[player][1][0] + 100
+
+        # si va vers le bas
+        elif(action > 0 and self.raquettePos[player][1][1] + action > self.topArea[1][0]):
+            self.raquettePos[player][1][1] = self.topArea[1][0]
+            # - taille de la raquette / à rajouter
+            self.raquettePos[player][1][0] = self.raquettePos[player][1][2] - 100
+
+        # la raquette ne touchera pas, alors on applique l'action
+        else:
+            self.raquettePos[player][1][0] += action
+            self.raquettePos[player][1][1] += action
+
+    def newRound(self):
+        # gestion des rounds et remise a 0 l'or d'un point marqué
+        
+
+        # remize a 0 de la balle
+        self.ballVector = [choice([i for i in range(-8, 8) if i not in [0]]), choice(
+            [i for i in range(-8, 8) if i not in [0]]), self.defaultSpeed]
+        self.ballPos = [self.areaRange[0]/2, self.areaRange[1]/2]
+
+        # actualisation des donnéees
+        self.gameInfo()
+
+        # debug
+        # sleep(3)
+
+    def gameInfo(self):
+        # on envoie les infos info du jeu au client :
+        # au lancement ou bien en cas de spectateur
+
+        return {
+            'ballPos': self.ballPos,
+            'score': self.gameScore,
+            'gameArea': self.area,
+            'raquettes': self.raquettePos
+        }
--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from gameManagement import GameProcessus
 from fps_limiter import LimitFPS, FPSCounter
 
 
-
 __author__ = "Brian Perret"
 
 # Initialization and configuration of app
@@ -17,7 +16,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['DEBUG'] = True
-app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=360)
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=360)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
 
 #app.config['SQLALCHIMY_TRACK_MODIFICATIONS'] = False
@@ -26,7 +25,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
 Session(app)
 
 # start socketio
-socketio = SocketIO(app, async_mode=None, logger=True, engineio_logger=True)
+socketio = SocketIO(app, async_mode=None, logger=False, engineio_logger=False)
 
 # start db
 db = SQLAlchemy(app)
@@ -172,100 +171,140 @@ def yMousePos_recu(data):
     print('yMousePos: ' + str(data))
     socketio.emit('recuYMousePos', {'yMousePos': data})
 
-#info sur les player, il faudra les initialisé lorsque que l'on cliquera sur start ou autre
-#mais pour l'instant on initialise lors du démarage du serv
+# info sur les player, il faudra les initialisé lorsque que l'on cliquera sur start ou autre
+# mais pour l'instant on initialise lors du démarage du serv
 
-player1 = {'username' : '', 'score' : 0, 'cursorPos' : 0}
-player2 = {'username' : '', 'score' : 0, 'cursorPos' : 0}
+
+player1 = {'username': ''}
+player2 = {'username': ''}
 gameStatu = False
+
+
 @app.route('/pong')
 def pong():
 
-
-    """
-    fonction playerInfo: permet de syncro les données entres le serveurs et les différents client
-    """
-    
-
     def playerInfo():
+        """
+        fonction qui permet de renseigner le nombre et nom des joueurs pret a joué
+
+
+        2 places uniquement accessible pour des connectés
+
+        les joueurs qui ne clique pas sur 'pret' son spectateur
+
+        """
         global gameStatu
 
-        
-        #on recup les infos des player du serveur
+        # on recup les infos des player du serveur
         #global player1
         #global player2
         #global gameStatu
 
+        # envoie l'état du nombre de pret
+        socketio.emit(
+            'statuGame', {'player1Info': player1, "player2Info": player2})
 
-        socketio.emit('statuGame', {'player1Info': player1, "player2Info" : player2})
-
-    
-        if(player1['username'] != "" and player2['username'] != "" and gameStatu == False, gameStatu != True):
+        # cas de deux joueurs qui sont prets -> lancement du jeu
+        if(player1['username'] != "" and player2['username'] != "" and gameStatu == False):
             gameStatu = True
-            print("jeu tourné sur on")    
+            print("jeu tourné sur on")
 
             socketio.sleep(1)
             game()
- 
-        else:
 
+        """
+        #probleme quand y'a un spec -> stop le jeu
+        else:
             print(player1['username'], player2['username'], gameStatu)
             gameStatu = False
             print("jeu tourné sur off")
-    
+        """
+
+    # envoie l'état du jeu lors du chargement de la page
     @socketio.on('connexion_serveur')
     def connexion_serveur():
         playerInfo()
-    
+
+    # gestion du joueur 1 isReady
     @socketio.on('readyPlayer1')
-    def readyPlayer1():
+    def isReadyPlayer1():
         global player1
         global player2
+
+        # check si utulisateur connecté
         if session.get("username"):
+            # check si place vide et check doublon
             if(player2['username'] != session.get("username") and player1['username'] == ""):
-                
+
+                # on save le pseudo du joueur 1
                 player1['username'] = session.get("username")
 
+                # on actualise les données de statu
                 playerInfo()
-                
 
-                #socketio.emit('readyPlayer1Info', {'username': player1})
+    # gestion du joueur 2 isReady
 
     @socketio.on('readyPlayer2')
-    def readyPlayer2():
+    def isReadyPlayer2():
         global player1
         global player2
+
+        # check si utulisateur connecté
         if session.get("username"):
+            # check si place vide et check doublon
             if(player1['username'] != session.get("username") and player2['username'] == ""):
-                
+
+                # on save le pseudo du joueur 2
                 player2['username'] = session.get("username")
 
+                # on actualise les données de statu
                 playerInfo()
-
 
     def game():
         #socketio.emit('statuGame', {'player1Info': player1, "player2Info" : player2})
 
-        #Game started
-        party = GameProcessus([500,500], 10)
-        
+        # Game started
+        party = GameProcessus([500, 500])
+
         fps_limiter = LimitFPS(fps=30)
         fps_counter = FPSCounter()
-        
 
-        print("SALUT !!!!")
-        print(gameStatu)
-        
+        print('gameStatu : ', gameStatu)
+
+        # cadenser le jeu a un certain nombre fixe de fps
         while (gameStatu):
             if fps_limiter():
 
-                ballPos = party.newBallPosition()
+                # recup les actions des joueurs
 
-                socketio.emit('gameInfo', {'ballPos' : ballPos})
+                @socketio.on('ArrowLeft')
+                def raquetteAction():
+                    if(session.get("username") == player1['username']):
+                        party.moveRaquette(0, -50)
+                    elif(session.get("username") == player2['username']):
+                        party.moveRaquette(1, -50)
+
+                @socketio.on('ArrowRight')
+                def raquetteAction():
+                    if(session.get("username") == player1['username']):
+                        party.moveRaquette(0, 50)
+                    elif(session.get("username") == player2['username']):
+                        party.moveRaquette(1, 50)
+
+                # debug :
+                party.newBallPosition()
+
+                # on recup les infos du jeu
+                data = party.gameInfo()
+
+                socketio.emit('gameInfo',  data)
                 socketio.sleep(0.01)
 
-                print("current fps: %s" % fps_counter())
-        
+                #ballPos = party.newBallPosition()
+                #socketio.emit('gameInfo', {'ballPos' : ballPos})
+
+                #print("current fps: %s" % fps_counter())
+
     return render_template('pong_test.html')
 
 #socketio.emit('message_to_send', {"user" : "anonymous", 'msg' : data})
